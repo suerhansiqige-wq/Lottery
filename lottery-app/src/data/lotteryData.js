9b2220a3cf7e202c3590f9d2bfede1c1db565a73
toolvx <admin@toolvx.com>
2026-09-12 21:30:06 +0800

    chore(data): 同步最新开奖数据（26245）

--- lottery-app/src/data/lotteryData.js
+++ lottery-app/src/data/lotteryData.js
@@ -8688,6 +8688,7 @@ export const lotteryData = [
   { issue: '26242', d1: 5, d2: 5, d3: 5 },
   { issue: '26243', d1: 2, d2: 1, d3: 9 },
   { issue: '26244', d1: 6, d2: 5, d3: 4 },
+  { issue: '26245', d1: 5, d2: 4, d3: 9 },
 ];
 
 export function enrichData(data) {
